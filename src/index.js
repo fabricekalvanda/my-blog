ad1ee6040fe1f8af14bf5b13616cba4e85ed591d
FabriceKalvanda <67396845+fabricekalvanda@users.noreply.github.com>
2023-10-18 15:02:08 -0700

    adds firebase and create acount pages

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBAtKY-N8-GC7_1UlavwV4xoCgrotYjGt0",
+  authDomain: "my-react-blog-7d762.firebaseapp.com",
+  projectId: "my-react-blog-7d762",
+  storageBucket: "my-react-blog-7d762.appspot.com",
+  messagingSenderId: "302721503853",
+  appId: "1:302721503853:web:7237eae8b23f3b693ce564"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
