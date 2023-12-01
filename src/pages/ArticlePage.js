c673078ade234b2f898b6c40cf9235c9bf287e0b
fabricekalvanda <fabricekalvanda@gmail.com>
2023-12-01 11:19:17 -0800

    add create account functionality in frontend

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -4,11 +4,15 @@ import axios from 'axios';
 import NotFoundPage from "./NotFoundPage";
 import CommentsList from "../components/CommentsList";
 import AddCommentForm from "../components/AddCommentForm";
+import userUser from "../hooks/useUser";
 import articles from "./article-content";
 
 const ArticlePage = () => {
     const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: []} );
     const { articleId } = useParams(); //Get the article id from the URL
+
+    const { user, isLoading } = userUser();
+
 
     useEffect(() => {
         const loadArticleInfo = async () => {
@@ -36,15 +40,21 @@ const ArticlePage = () => {
         <>
         <h1>{article.title}</h1>
         <div className="upvotes-section">
-            <button onClick={addUpvote}>Upvote</button>
+            {user
+                ? <button onClick={addUpvote}>Upvote</button>
+                : <button>Log in to upvote</button>
+            }
             <p>This article has {articleInfo.upvotes} upvote(s)</p>
         </div>
         {article.content.map((paragraph, i) => (
             <p key={i}>{paragraph}</p>
         ))}
-        <AddCommentForm
-            articleName={articleId}
-            onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
+        {user
+            ? <AddCommentForm
+                articleName={articleId}
+                onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
+            : <button>Log in to add a comments</button>
+        }
         <CommentsList comments={articleInfo.comments} />
         </>
         
